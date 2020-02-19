a60d8a4e5a731b8493919a1cb3214a74141ad5c5
NikNikich <43465350+NikNikich@users.noreply.github.com>
2020-02-19 18:57:36 +0300

    token error WTF In Home end toMorrow

---
+++ src/main/kotlin/com/example/kotlinlearn/service/JwtService.kt
@@ -0,0 +1,37 @@
+package com.example.kotlinlearn.service
+
+
+//import java.time.temporal.TemporalUnit
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.time.temporal.TemporalUnit
+import java.util.*
+
+@Component
+class JwtService() {
+    private val algorithm: Algorithm=Algorithm.HMAC256("KuKaReKu")
+    fun create(role: String?, id: Long?, secret: String?): String {
+        return JWT.create().withClaim(ID_CLAIM, id)
+                .withClaim(SECRET_CLAIM, secret).sign(algorithm)
+    }
+
+    fun verify(jwt: String?): DecodedJWT {
+        return JWT.require(algorithm).build().verify(jwt)
+    }
+
+    fun expireAfter(expireAfter: Long, unit: TemporalUnit?): Date {
+        return Date.from(Date().toInstant().plus(expireAfter, unit))
+    }
+
+    companion object {
+        const val ROLE_CLAIM = "ROLE"
+        const val ID_CLAIM = "ID"
+        const val CODE_CLAIM = "CODE"
+        const val SECRET_CLAIM = "SECRET"
+    }
+
+
+}
